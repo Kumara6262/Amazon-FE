91e245b2023b03bbe868007eeafebb40724c3df2
Rakshith <131443852+Kumara6262@users.noreply.github.com>
2025-09-16 23:42:15 +0530

    Update Main.tf

--- JENKINS-TF/Main.tf
+++ JENKINS-TF/Main.tf
@@ -1,13 +1,14 @@
 resource "aws_security_group" "Jenkins-sg" {
-  name        = "Jenkins-Security Group"
+  name        = "Jenkins-Security-Group"
   description = "Open 22,443,80,8080,9000"
 
-  # Define a single ingress rule to allow traffic on all specified ports
-  ingress = [
-    for port in [22, 80, 443, 8080, 9000, 3000] : {
-      description      = "TLS from VPC"
-      from_port        = port
-      to_port          = port
+  # Define ingress rules using dynamic block
+  dynamic "ingress" {
+    for_each = [22, 80, 443, 8080, 9000, 3000]
+    content {
+      description      = "Allow port ${ingress.value}"
+      from_port        = ingress.value
+      to_port          = ingress.value
       protocol         = "tcp"
       cidr_blocks      = ["0.0.0.0/0"]
       ipv6_cidr_blocks = []
@@ -15,7 +16,7 @@ resource "aws_security_group" "Jenkins-sg" {
       security_groups  = []
       self             = false
     }
-  ]
+  }
 
   egress {
     from_port   = 0
@@ -29,10 +30,9 @@ resource "aws_security_group" "Jenkins-sg" {
   }
 }
 
-
 resource "aws_instance" "web" {
-  ami                    = "ami-0f5ee92e2d63afc18"
-  instance_type          = "t2.large"
+  ami                    = "ami-0f5ee92e2d63afc18"  # Ubuntu 20.04 LTS
+  instance_type          = "t3.micro"              # Free tier eligible t3.micro
   key_name               = "Mumbai"
   vpc_security_group_ids = [aws_security_group.Jenkins-sg.id]
   user_data              = templatefile("./install_jenkins.sh", {})
@@ -40,7 +40,8 @@ resource "aws_instance" "web" {
   tags = {
     Name = "Jenkins-sonar"
   }
+  
   root_block_device {
-    volume_size = 30
+    volume_size = 8  # Reduced to 8GB to stay within free tier limits
   }
 }
