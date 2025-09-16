c43a6ca7e207eb9eb5322493c550a46640e571f7
Ubuntu <ubuntu@ip-172-31-17-103.ap-south-1.compute.internal>
2025-09-16 16:17:37 +0000

    Updated backend.tf configuration

--- EKS/backend.tf
+++ EKS/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "ravi-project-s3bucket"  # Replace with your actual S3 bucket name
+    bucket = "rakshith-s3-bucket-2024"  # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"  
     region = "ap-south-1" 
   }
